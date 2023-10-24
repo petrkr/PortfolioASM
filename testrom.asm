d8c778de660dde92450f6fd9a7570f55150605cf
Petr Kracík <petrkr@petrkr.net>
2023-10-25 00:15:34 +0200

    Test ROM code
    Signed-off-by: Petr Kracík <petrkr@petrkr.net>

---
+++ testrom.asm
@@ -0,0 +1,82 @@
+name        XROM
+
+            assume cs:cseg
+
+LF          equ     0aH
+CR          equ     0dH
+
+DOSX        equ     055aaH
+
+cseg        segment
+
+            org 0H
+
+xrom_main   label   near
+bixt_type   dw      DOSX
+bixt_size   db      0 ; num 512 byte blocks in ROM
+
+            org 3H
+bixt_gdos   label   byte
+            jmp     genx_vect
+
+
+            org 40H
+bixt_user   label   byte
+            db      'Test ROM'
+
+xrom_proc   proc    FAR
+
+genx_vect   label   near
+            push    bp
+            push    ax
+            push    bx
+            push    cx
+            push    dx
+            push    es
+
+            mov     bp,offset   hello_text   ; Hello text
+            call    disp_text
+
+            mov     bp,offset   end_text
+            call    disp_text
+
+            pop     es
+            pop     dx
+            pop     cx
+            pop     bx
+            pop     ax
+            pop     bp
+
+            ret
+
+xrom_proc   endp
+
+disp_text   proc    near
+            xor     bh, bh
+            mov     ah, 3
+            int     10H
+
+            push    cs
+            pop     es
+
+            xor     ch, ch
+            mov     cl, es:[bp]
+            inc     bp
+
+            mov     ax, 1301H
+            int     10H
+
+            ret
+disp_text   endp
+
+
+hello_text  db      end_text-$-1
+            db      'Hello from ROM Code', CR, LF
+
+end_text    db      crlf_text-$-1
+            db      'End ROM code, Return to system', CR, LF
+
+crlf_text   db      2,CR,LF
+
+cseg        ends
+            end     xrom_main
